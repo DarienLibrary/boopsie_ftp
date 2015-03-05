0d4ee2f714a8c7e0c2cef9fcbbf2aff50cc32b1a
James McNutt <james.ryan.mcnutt@gmail.com>
2015-03-05 15:39:05 -0500

    clean up
    cleaner code, cleans up after itself and minimal logging

--- boopsie_ftp.py
+++ boopsie_ftp.py
@@ -1,5 +1,5 @@
 import os
-from datetime import date
+from datetime import date, datetime
 import zipfile
 from ftplib import FTP
 
@@ -8,14 +8,13 @@ host = os.environ.get('boopsie_host')
 user = os.environ.get('boopsie_user')
 password = os.environ.get('boopsie_password')
 local_directory = r'C:\ProgramData\Polaris\SrServiceRoot\hermione\18\scheduledjobs'
-remote_directory = 'darien'
 filename_trunc = 'bib_Boopsie Full MARC_{}'.format(date.today().strftime('%m%d%Y'))
+logfile = 'log_boobsie.txt'
 filename = ''
 for f in os.listdir(local_directory):
     if f.startswith(filename_trunc): filename = f
     
 def zip_file(filename, directory):
-    full_filename = '{}\\{}'.format(directory, filename)
     zip_filename = filename.split('.')[0]+'.zip'
     zf = zipfile.ZipFile(zip_filename, mode='w')
     try:
@@ -25,16 +24,23 @@ def zip_file(filename, directory):
     return zip_filename
 
 def ftp_put(host, user, password, filename, directory):
-    full_filename = '{}\\{}'.format(directory, filename)
+    full_filename = r'{}\{}'.format(directory, filename)
     ftp = FTP(host, user, password)
-    ftp.cwd(remote_directory)
+    ftp.cwd('darien')
     with open(full_filename, 'rb') as f:
         ftp.storbinary('STOR {}'.format(filename), f)
+    with open(logfile, 'a') as f:
+        f.write('{}: {} uploaded from {}'.format(datetime.now(), filename, directory))
 
 if filename:
+    full_filename = r'{}\{}'.format(local_directory, filename)
     if zipped:
-        filename = zip_file(filename, local_directory)
-        local_directory = '.'
-    ftp_put(host, user, password, filename, local_directory)
-    if zipped:
-        os.remove(filename)
+        zip_filename = zip_file(filename, local_directory)
+        ftp_put(host, user, password, zip_filename, '.')
+        os.remove(zip_filename)
+    else:
+        ftp_put(host, user, password, filename, local_directory)
+    os.remove(full_filename)
+else:
+    with open(logfile, 'a') as f:
+        f.write('{}: File not found to upload'.format(datetime.now()))
